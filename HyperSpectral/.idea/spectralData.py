95ee262d765773dcd9d51f3908e76ef98a9cd925
Michael Byrd <mcb4548@uncw.edu>
2017-07-18 12:01:27 -0400

    Initial Commit

--- HyperSpectral/.idea/spectralData.py
+++ HyperSpectral/.idea/spectralData.py
@@ -7,6 +7,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.datasets import make_classification
+from sklearn.ensemble import ExtraTreesClassifier
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Load dataset
 
@@ -14,7 +18,7 @@ df = pd.read_csv('C:\\Users\Michael Byrd\Documents\StapletonLab\headers3mgperml.
 dfValues = df.values
 
 # Observations
-x = df.iloc[:, 15:]
+X = df.iloc[:, 15:]
 
 # Responses Split
 y_cat, y_num = pd.get_dummies(pd.DataFrame(df.iloc[:, 1:5])), df.iloc[:, 5:13].drop('PKID', 1, inplace=False)
@@ -31,11 +35,16 @@ y = pd.concat((y_cat, y_num), axis=1)
 #     knn.fit(X_train, y_train)
 #     y_pred = knn.predict(X_test)
 #     print(metrics.accuracy_score(y_test, y_pred), i)
-# regr = DecisionTreeRegressor(max_dept.
 
-
-print(lipids.columns)
 
 for col in lipids.columns:
     for index, row in lipids.iterrows():
-        print(col, index, lipids[col][index])
+        lipids.set_value(index, col, int(lipids[col][index] * (10 ** 6)))
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, lipids)
+forest = ExtraTreesClassifier()
+forest.fit(X_train, y_train)
+
+
